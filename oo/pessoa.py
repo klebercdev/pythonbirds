c21251b45593f26197d87383f3dc5bdb332c17b9
Kleber <mk.kleber@gmail.com>
2021-12-23 22:30:29 -0300

    Criados atributo de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=41):
+        self.idade = idade
+        self.nome = nome
+   
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Silva')
     print(Pessoa.cumprimentar(p))
     print(p.cumprimentar())
     print(id(p))
+    print(p.nome)
+    p.nome = 'Kleber'
+    print(p.nome)
